69e5f5e2c155c309c592bb6c27270e6833cbe5c2
KeepinItReal1 <36188151+KeepinItReal1@users.noreply.github.com>
2019-11-06 18:15:13 +0200

    Initial commit

---
+++ ProjectTesseract/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System.Drawing;
+
+namespace ProjectTesseract
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.ConvertImageToText();
+        }
+        public void ConvertImageToText()
+        {
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.Lithuanian);
+                string plainText = api.GetTextFromImage("C:\\Users\\justas\\source\\repos\\ProjectTesseract\\ProjectTesseract\\Test3.jpg");
+    
+                Console.WriteLine(plainText);
+                Console.Read();
+            }
+        }
+    }
+}
